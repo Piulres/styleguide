a582a484194a7f199ca0362cd9646789d787d86c
Renan Emigdio <renanemigdiogoncalves@hotmail.com>
2017-03-17 16:00:58 -0300

    v1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 $( document ).ready(function() {
 	
+	$(function () {
+	  $('[data-toggle="tooltip"]').tooltip()
+	})
+
+	$(function () {
+	  $('[data-toggle="popover"]').popover()
+	})
+		
+	$( ".toggle-page-1" ).click(function() {
+	  $(".page-1").toggleClass( "display" );
+	});
+
+	$( ".toggle-page-2" ).click(function() {
+	  $(".page-2").toggleClass( "display" );
+	});
+
+	$( ".toggle-page-3" ).click(function() {
+	  $(".page-3").toggleClass( "display" );
+	});
+
+	$( ".toggle-page-4" ).click(function() {
+	  $(".page-4").toggleClass( "display" );
+	});
 });
