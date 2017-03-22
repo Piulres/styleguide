ee0f6b1a66e5a0f82d6d3031d3c4a346b715d69e
Renan Emigdio <renanemigdiogoncalves@hotmail.com>
2017-03-22 17:03:05 -0300

    v2

--- js/script.js
+++ js/script.js
@@ -10,12 +10,6 @@ $( document ).ready(function() {
 	  $('[data-toggle="popover"]').popover()
 	})
 	
-	// Toggle Page Live Preview 
-	$( ".toggle-page-1, .toggle-page-2, .toggle-page-3, .toggle-page-4" ).click(function() {
-		$(this).toggleClass('active');
-	});
-
-
 	// datepicker
 	$('.input-datepicker').datepicker()
 	.on('changeDate', function(ev){
