81d7ae55144140879c202cd13b6d54691b4c6280
fiote <murilo.eng@gmail.com>
2017-09-29 13:32:34 -0300

    finalizando menu lateral e header

--- js/script-mobile.js
+++ js/script-mobile.js
@@ -1,3 +1,24 @@
 $(document).on('click','#searchIcon',function() {
-    $('header').toggleClass('searching');
+	var $search = $('#searchIcon');
+	if ($search.hasClass('active')) {
+		$search.removeClass('active');
+		$('.show-searching').hide();
+		$('.hide-searching').fadeIn();
+	} else {
+		$search.addClass('active');
+		$('.hide-searching').hide();
+		$('.show-searching').fadeIn();
+		$('#searchTop').focus();
+
+	}
+});
+
+$(document).on('click','#btnSideMenu',function() {
+	$('#menu').css('left','-100%').show().animate({'left':'0%'});
+	$(document.body).addClass('menu-open');
+});
+
+$(document).on('click','#btnCloseMenu',function() {
+	$('#menu').animate({'left':'-100%'},function() { $('#menu').hide(); });
+	$(document.body).removeClass('menu-open');
 });
